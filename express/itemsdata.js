b408be444beef8e9f7332870d8b77d94926fae21
Saifuddin <you@example.com>
2021-10-23 12:24:50 -0400

    First changes

---
+++ express/itemsdata.js
@@ -0,0 +1,161 @@
+/* <script type="text/javascript">
+data = [_size];
+data = [
+    {
+        "name"= 'Butter Chicken with Rice and Vegetables',
+        "category"= 'Weight Loss',
+        "price"= '$8',
+        "image"= 'images/weightloss1.jpg',
+        "itemNumber"= 1 
+    },
+    {
+        "name"= 'Coconut Curry Shrimp with Rice and Vegetables',
+        "category"= 'Weight Loss',
+        "price"= '$12',
+        "image"= 'images/weightloss2.jpg',
+        "itemNumber"= 2
+    },
+    {
+        "name"= 'Roast Chicken with Mushroom Demi gravy',
+        "category"= 'Weight Loss',
+        "price"= '$12',
+        "image"= 'images/weightloss3.jpg',
+        "itemNumber"= 3
+    },
+    {
+        "name"= 'Butter Chicken with Rice and Vegetables',
+        "category"= 'Muscle Gain',
+        "price"= '$8',
+        "image"= 'images/weightloss1.jpg',
+        "itemNumber"= 4
+    },
+    {
+        "name"= 'Coconut Curry Shrimp with Rice and Vegetables',
+        "category"= 'Muscle Gain',
+        "price"= '$12',
+        "image"= 'images/weightloss2.jpg',
+        "itemNumber"= 5
+    },
+    {
+        "name"= 'Roast Chicken with Mushroom Demi gravy',
+        "category"= 'Muscle Gain',
+        "price"= '$12',
+        "image"= 'images/weightloss3.jpg',
+        "itemNumber"= 6
+    },
+    {
+        "name"= 'KETO Tomato and Basil Pesto Soup',
+        "category"= 'Keto',
+        "price"= '$8',
+        "image"= 'images/keto1.jpg',
+        "itemNumber"= 7
+    },
+    {
+        "name"= 'KETO Chicken Crabonara Bowl',
+        "category"= 'Keto',
+        "price"= '$12',
+        "image"= 'images/keto2.jpg',
+        "itemNumber"= 8
+    },
+    {
+        "name"= 'Chicken Carbonara Bowl',
+        "category"= 'Fat Burner',
+        "price"= '$12',
+        "image"= 'images/keto2.jpg',
+        "itemNumber"= 9
+    },
+    {
+        "name"= 'Garlic Butter Salmon with Vegetables',
+        "category"= 'Fat Burner',
+        "price"= '$12',
+        "image"= 'images/fatburner1.jpg',
+        "itemNumber"= 10
+    },
+    {
+        "name"= 'Roast Beef with Mushroom Demi gravy',
+        "category"= 'Fat Burner',
+        "price"= '$12',
+        "image"= 'images/fatburner2.jpg',
+        "itemNumber"= 11
+    }
+];
+</script> */
+const data = [
+    {
+        name: 'Butter Chicken with Rice and Vegetables',
+        category: 'Weight Loss',
+        price: '$8',
+        image: 'images/weightloss1.jpg',
+        itemNumber: 1 
+    },
+    {
+        name: 'Coconut Curry Shrimp with Rice and Vegetables',
+        category: 'Weight Loss',
+        price: '$12',
+        image: 'images/weightloss2.jpg',
+        itemNumber: 2
+    },
+    {
+        name: 'Roast Chicken with Mushroom Demi gravy',
+        category: 'Weight Loss',
+        price: '$12',
+        image: 'images/weightloss3.jpg',
+        itemNumber: 3
+    },
+    {
+        name: 'Butter Chicken with Rice and Vegetables',
+        category: 'Muscle Gain',
+        price: '$8',
+        image: 'images/weightloss1.jpg',
+        itemNumber: 4
+    },
+    {
+        name: 'Coconut Curry Shrimp with Rice and Vegetables',
+        category: 'Muscle Gain',
+        price: '$12',
+        image: 'images/weightloss2.jpg',
+        itemNumber: 5
+    },
+    {
+        name: 'Roast Chicken with Mushroom Demi gravy',
+        category: 'Muscle Gain',
+        price: '$12',
+        image: 'images/weightloss3.jpg',
+        itemNumber: 6
+    },
+    {
+        name: 'KETO Tomato and Basil Pesto Soup',
+        category: 'Keto',
+        price: '$8',
+        image: 'images/keto1.jpg',
+        itemNumber: 7
+    },
+    {
+        name: 'KETO Chicken Crabonara Bowl',
+        category: 'Keto',
+        price: '$12',
+        image: 'images/keto2.jpg',
+        itemNumber: 8
+    },
+    {
+        name: 'Chicken Carbonara Bowl',
+        category: 'Fat Burner',
+        price: '$12',
+        image: 'images/keto2.jpg',
+        itemNumber: 9
+    },
+    {
+        name: 'Garlic Butter Salmon with Vegetables',
+        category: 'Fat Burner',
+        price: '$12',
+        image: 'images/fatburner1.jpg',
+        itemNumber: 10
+    },
+    {
+        name: 'Roast Beef with Mushroom Demi gravy',
+        category: 'Fat Burner',
+        price: '$12',
+        image: 'images/fatburner2.jpg',
+        itemNumber: 11
+    }
+]
---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("express"));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/express/index.html');
+})
+
+app.get('/packages', (req, res) => {
+    res.sendFile(__dirname + '/express/packages.html');
+})
+
+app.get('/account', (req, res) => {
+    res.sendFile(__dirname + '/express/account.html');
+})
+
+const port = 3000;
+
+var listener = app.listen(port, function() {
+    console.log('Server listening on port ' + port);
+});
